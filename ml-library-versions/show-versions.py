f301955233d11153492e439ee9ce9e1780f2b4e0
Christian Blab <chris.blab@gmail.com>
2017-08-05 01:43:05 +0200

    installed  a few ml libraries.

---
+++ ml-library-versions/show-versions.py
@@ -0,0 +1,41 @@
+# scipy
+import scipy
+print('scipy: %s' % scipy.__version__)
+# numpy
+import numpy
+print('numpy: %s' % numpy.__version__)
+# matplotlib
+import matplotlib
+print('matplotlib: %s' % matplotlib.__version__)
+# pandas
+import pandas
+print('pandas: %s' % pandas.__version__)
+# statsmodels
+import statsmodels
+print('statsmodels: %s' % statsmodels.__version__)
+# scikit-learn
+import sklearn
+print('sklearn: %s' % sklearn.__version__)
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
+
+'''
+/usr/bin/python3.5 /home/cblab/PycharmProjects/py-sandbox/ml-library-versions/ml-library-versions.py
+scipy: 0.19.1
+numpy: 1.13.1
+matplotlib: 2.0.2
+pandas: 0.20.3
+statsmodels: 0.8.0
+sklearn: 0.18.2
+theano: 0.9.0
+Using TensorFlow backend.
+tensorflow: 1.2.1
+keras: 2.0.6
+'''
